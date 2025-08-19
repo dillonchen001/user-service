210a3ee7c8e3af15a854e24b33da94d92edddc4d
chenhao <bjbd2015@163.com>
2025-08-20 02:06:35 +0800

    edit user_id

--- internal/data/auth_provider.go
+++ internal/data/auth_provider.go
@@ -31,6 +31,7 @@ func (r *authProviderRepo) Create(ctx context.Context, proType, id string, userI
 		SetProviderType(proType).
 		SetProviderID(id).
 		SetUser(userInfo).
+		SetUserID(userInfo.UserID).
 		Save(ctx)
 
 	return err
